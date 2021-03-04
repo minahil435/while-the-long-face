9ed97c2e0d006070dd0de3143a787e3693b75dc9
Minahil shafique <minahal.shafique@gmail.com>
2021-03-04 14:21:43 -0500

    completed functions

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 function numberDoubler(num) {
     // Your code here
+    while(num <= 100){
+        num = num * 2
+    }
+return num
 }
 
 function stringRepeater(str) {
     // Your code here
+    let original = str
+    while(str.length < 10){
+        str = str + original
+    }
+    return str
 }
 
 function makeDivisible(x, y) {
     // Your code here
+    while (x%y !== 0){
+            x = x + 1
+    }
+    return x
+
 }
 
 module.exports = {
